9c7f55d71bf4d9785e92d1ce3afbada1d8aa7a3c
noellelaw <noelle.t.law@gmail.com>
2024-07-01 22:50:35 -0400

    adding a few more hobbies to the mix

--- pages/7_Hobbies.py
+++ pages/7_Hobbies.py
@@ -19,17 +19,22 @@ with st.container():
     col1,col2, col3, col4 = st.columns([2,2,2,2])
     with col1: 
       st.image("images/art1.jpeg")
-      st.image(("images/running1.jpeg"))
+      st.image("images/running1.jpeg")
       st.image("images/hiking1.png")
+      st.image("images/soccer.jpeg")
     with col2: 
       st.image("images/art2.jpeg")
       st.image("images/hiking2.jpeg")
+      st.image("images/hiking7.JPG")
+      st.image("images/hiking8.jpeg")
     with col3: 
       st.image(("images/running2.jpeg"))
       st.image("images/art3.jpeg")
       st.image("images/art6.png")
+      st.image("images/hiking5.jpeg")
     with col4: 
       st.image("images/hiking3.jpeg")
       st.image("images/art4.jpeg")
       st.image("images/art7.png")
       st.image("images/art5.png")
+      st.image("images/hiking6.jpeg")
