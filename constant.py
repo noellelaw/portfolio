448527baf3ed99ff4eab9d1402f5c60e5c5fa982
noellelaw <noelle.t.law@gmail.com>
2024-10-15 21:50:10 -0400

    Constant bug

--- constant.py
+++ constant.py
@@ -4,7 +4,6 @@ info = {
    "Name": "Noelle",
    "Full_Name": "Noelle Law",
    "Intro": "A Machine Learning and Perception Engineer @ JHU Applied Physics Lab",
-   "About":"**I am passionate about creating more interpretable and trustworthy machine learning systems for healthcare and sustainability applications.** As a Machine Learning & Perception Engineer at JHUAPL, my research focuses on enhancing safety and robustness through uncertainty estimation, generative AI, visualization, human-machine interaction, and rigorous testing and evaluation. I have degrees in computer science and engineering, experience in both low- and high-level programming languages, and specialities in machine learning and design integration. Explore my portfolio to discover more about my work!",
-   "City":"Baltimore, Maryland",
+   "City":"Baltimore, Maryland",   "About":"**I am passionate about creating more interpretable and trustworthy machine learning systems for healthcare and sustainability applications.** As a Machine Learning & Perception Engineer at JHUAPL, my research focuses on enhancing safety and robustness through uncertainty estimation, generative AI, human-machine interaction, and rigorous testing and evaluation. I have degrees in computer science and engineering, experience in both low- and high-level programming languages, and specialities in machine learning and design integration. Explore my portfolio to discover more about my work!",
    "Email": "noelle.t.law@gmail.com"
 }
